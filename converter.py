e49361bac2ea59a321d164a53432a7543a47314e
Jasper <jasper@vanderstoop.nl>
2019-12-17 08:48:08 +0100

    docs: add comments

--- converter.py
+++ converter.py
@@ -7,27 +7,36 @@ class TelligentWidget(object):
     """A Telligent Widget XML to source file decoder/encoder"""
 
     def decode(self, input, output=False):
+        # Check if input file exist
         if not os.path.isfile(input):
             sys.exit(f"File '{input}' does not exist.")
 
-        if not output:
-            output = input.split('.')[0]
-
-        if os.path.exists(output) is False:
-            os.mkdir(output)
-
+        # Check if input file contains valid XML
+        # TODO: check if valid Telligent Widget
         try:
             tree = ET.parse(input)
         except ET.ParseError:
             exit("Invalid XML")
 
+        # Set fallback output folder to equal the name of the widget
+        if not output:
+            output = input.split('.')[0]
+
+        # Create output folder
+        if os.path.exists(output) is False:
+            os.mkdir(output)
+
+        # Start parsing the widget XML
         root = tree.getroot()
 
+        # Decode contents of each file and save it in output folder
         for file in root.findall(".//file"):
             contents_encoded = file.text
             contents = base64.b64decode(contents_encoded)
+
             file_name = file.get("name")
             output = "/".join((output, file_name))
+
             f = open(output, "wb")
             f.write(contents)
             f.close()
